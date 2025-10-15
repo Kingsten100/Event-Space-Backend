1484b2f1902d5a0ddf2482be5947c81e2d9ea385
Kingsten100 <isak9910@gmail.com>
2025-10-15 18:48:03 +0200

    Added update listing functionality

--- src/controllers/listing.controller.js
+++ src/controllers/listing.controller.js
@@ -1,6 +1,7 @@
 import AsyncHandler from "express-async-handler";
 import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
+import mongoose from "mongoose";
 
 // ==== CREATE LISTING ==== //
 export const createListing = AsyncHandler(async(req, res) => {
@@ -41,7 +42,6 @@ const user = await User.findById(userId);
 })
 
 // ==== GET LISTINGS ==== //
-
 export const getListings = AsyncHandler(async(req, res) => {
 
   const listings = await Listing.find()
@@ -50,3 +50,66 @@ export const getListings = AsyncHandler(async(req, res) => {
  
 })
 
+// ==== GET LISTING BY ID ==== //
+export const getListingById = AsyncHandler(async(req, res) => {
+
+  const listingId = req.params.id
+
+  if (!mongoose.Types.ObjectId.isValid(listingId)) {
+  return res.status(400).json({ message: "Invalid listing Id" });
+}
+
+  const listing = await Listing.findById(listingId)
+
+  res.status(200).json(listing)
+})
+
+// ==== GET MY LISTINGS ==== //
+export const getMyListings = AsyncHandler(async(req, res) => {
+
+  const userId = req.user._id
+
+  const listings = await Listing.find({ user: userId})
+
+  if(!listings || listings.length === 0){
+    return res.status(404).json({ message: 'No listings found for this user'})
+  }
+
+  res.status(200).json(listings)
+ 
+})
+
+// ==== UPDATE MY LISTING === //
+export const updateMyListing = AsyncHandler(async(req, res) => {
+  const {id} = req.params
+  const {title, description, address, location, amenities, price, images, capacity, category, rules} = req.body
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(400).json({ message: 'Invalid listing Id'})
+  }
+
+  const toUpdate = {}
+
+  if(title) toUpdate.title = title
+  if(description) toUpdate.description = description
+  if(address) toUpdate.address = address
+  if(location) toUpdate.location = location
+  if(amenities) toUpdate.amenities = amenities
+  if(price) toUpdate.price = price
+  if(images) toUpdate.images = images
+  if(capacity) toUpdate.capacity = capacity
+  if(category) toUpdate.category = category
+  if(rules) toUpdate.rules = rules
+  
+  if(Object.keys(toUpdate).length === 0){
+    res.status(400).json({ message: 'No changes provided' })
+  }
+
+  const updatedListing = await Listing.findByIdAndUpdate(id, toUpdate, {new: true})
+
+  if(!updatedListing){
+    return res.status(404).json({ message: 'Can not find that listing'})
+  }
+
+  res.status(200).json(updatedListing)
+})
--- src/routes/listing.route.js
+++ src/routes/listing.route.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import { verifyToken } from '../middleware/auth.middleware.js'
-import { createListing, getListings } from '../controllers/listing.controller.js'
+import { createListing, getListingById, getListings, getMyListings, updateMyListing } from '../controllers/listing.controller.js'
 
 const router = express.Router()
 
 router.post('/create', verifyToken, createListing)
-router.get('/listings', getListings)
+
+router.get('/alllistings', getListings)
+router.get('/mylistings', verifyToken, getMyListings)
+
+router.patch('/:id/edit', verifyToken, updateMyListing)
+router.put('/:id/edit', verifyToken, updateMyListing)
+
+
+router.get('/:id', getListingById)
 
 export default router
