3f7a5efdcec69d53790c9124c2c0944845781c94
Kingsten100 <isak9910@gmail.com>
2025-10-25 17:49:39 +0200

    Frontend structure and axios instance built

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import userRoutes from './routes/user.route.js'
 import listingRoutes from './routes/listing.route.js'
 import bookingRoutes from './routes/booking.route.js'
 import reviewRoutes from './routes/review.route.js'
+import cors from 'cors'
+
 
 const app = express()
 
+app.use(cors({
+  origin: ["http://localhost:5173"], // eller flera origins om du vill
+  credentials: true, // om du använder cookies eller auth headers
+}));
 app.use(express.json())
 
 app.use('/api/auth', userRoutes)
